56a5b24bf04da614d967f26086e8bf307d0598f9
cryosx <cyruswu.email@gmail.com>
2018-05-03 15:48:29 -1000

    /products/new POST implemented

--- routes/products.js
+++ routes/products.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const knex = require('../db');
 
 router.route('/').get((req, res) => {
-  knex
+  return knex
     .raw('SELECT * FROM products', [])
     .then(resp => {
       let products = resp.rows;
@@ -16,10 +16,48 @@ router.route('/').get((req, res) => {
 
 router
   .route('/:product_id')
-  .get()
-  .put()
+  .get((req, res) => {
+    const product_id = req.params.product_id;
+    return knex
+      .raw('SELECT * FROM products WHERE id = ?', [product_id])
+      .then(resp => {
+        let rowCount = resp.rowCount;
+        if (rowCount !== 1) {
+          return res.status(404).json({ error: 'Product not found' });
+        }
+        let product = resp.rows[0];
+        return res.json(product);
+      })
+      .catch(err => {
+        return res.status(500).json({ error: err });
+      });
+  })
+  .put((req, res) => {})
   .delete();
 
-router.route('/new').post();
+router.route('/new').post((req, res) => {
+  const data = req.body;
+  const title = data.title.trim();
+  const description = data.description.trim();
+  const inventory = Number.parseInt(data.inventory);
+  const price = Number.parseFloat(data.price);
+
+  return knex
+    .raw(
+      'INSERT INTO products (title,description,inventory,price) VALUES(?,?,?,?) RETURNING *',
+      [title, description, inventory, price]
+    )
+    .then(resp => {
+      let rowCount = resp.rowCount;
+      if (rowCount !== 1) {
+        return res.status(404).json({ error: 'Must POST all product fields' });
+      }
+      let product = resp.rows[0];
+      return res.json(product);
+    })
+    .catch(err => {
+      return res.status(500).json({ error: err });
+    });
+});
 
 module.exports = router;
