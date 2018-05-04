e020adfe0d9c3205cb49e91d779f0a84f6b3d7b4
cryosx <cyruswu.email@gmail.com>
2018-05-03 15:22:26 -1000

    /products GET implemented

--- routes/products.js
+++ routes/products.js
@@ -1,8 +1,18 @@
 const express = require('express');
-
 const router = express.Router();
+const knex = require('../db');
 
-router.route('/').get();
+router.route('/').get((req, res) => {
+  knex
+    .raw('SELECT * FROM products', [])
+    .then(resp => {
+      let products = resp.rows;
+      return res.json(products);
+    })
+    .catch(err => {
+      return res.status(500).json({ error: err });
+    });
+});
 
 router
   .route('/:product_id')
