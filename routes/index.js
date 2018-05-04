e70ec8695f483708862d4e8fd2a20181fd83d4f1
cryosx <cyruswu.email@gmail.com>
2018-05-04 10:36:37 -1000

    added purchases route

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,13 @@ const express = require('express');
 const users = require('./users.js');
 const products = require('./products.js');
 const cart = require('./cart.js');
+const purchases = require('./purchases.js');
 
 const router = express.Router();
 
 router.use('/users', users);
 router.use('/products', products);
 router.use('/cart', cart);
+router.use('/purchases', purchases);
 
 module.exports = router;
